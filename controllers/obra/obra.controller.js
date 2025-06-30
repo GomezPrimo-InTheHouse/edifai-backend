5f8f7ed5abf6fc6df9f9358374ddcf8dcc609a62
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2025-06-29 23:28:04 -0300

    avances en obras

--- controllers/obra/obra.controller.js
+++ controllers/obra/obra.controller.js
@@ -189,23 +189,138 @@ const modifyObra = async (req, res) => {
 
 
 //dar de baja obra ( no eliminar)
-const deleteObra = async (req, res) => {
-   const { id } = req.params;
+const darDeBajaObra = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-   
+   //validar existencia de obra
+
+   const existeObra = await pool.query('Select * from Obras where id = $1', [id])
+   if (existeObra.rows[0] === 0) {
+    return res.status(404).json({ message: 'Obra no encontrada' });
+   }
+
+   //actualizar estado de obra
+   const result = await pool.query(`
+    UPDATE Obras
+    SET estado_id = 2
+    WHERE id = $1
+    RETURNING *;
+    `, [id]);
+
+    res.status(200).json({
+      success:true,
+      message: 'Obra dada de baja correctamente',
+      obra: result.rows[0]
+    })
+
+  } catch (error) {
+    console.error('Error al dar de baja obra:', error);
+    return res.status(500).json({ error: 'Error interno del servidor' });
+    
+  }
+      
 
 }
 
 //getObraByID
 const getObraByID = async (req, res) => {
-   
+   try {
+    const { id } = req.params;
+    const obra = await pool.query('SELECT * FROM Obras WHERE id = $1', [id])
+    if (obra.rows[0] === 0) {
+      return res.status(404).json({ message: 'Obra no encontrada' });
+      }
+      res.status(200).json({
+        success:true,
+        message: 'Obra encontrada correctamente',
+        obra: obra.rows[0]
+        })
+    
+   } catch (error) {
+    res.status(500).json({
+      success:false,
+      message: 'Error interno del servidor',
+      error: error.message
+    })
+   }
 }
+
+
+const getObrasByUbicacion = async (req, res) =>{
+  try {
+    const {ubicacion} = req.params
+
+    //buscar dentro de atributo 'ubicacion' si contiene la ubicacion obtenida por parametros
+    const obras = await pool.query('SELECT * FROM Obras WHERE ubicacion ILIKE $1', [`%${ubicacion}%`])
+    if (obras.rows[0] === 0) {
+      return res.status(404).json({ message: 'No hay obras en la ubicación solicitada'})
+    }
+
+    res.status(200).json({
+      success:true,
+      message: 'Obras encontradas correctamente',
+      obras: obras.rows
+    })
+
+  } catch (error) {
+
+    res.status(500).json({
+      success:false,
+      message: 'Error interno del servidor',
+      error: error.message
+    })
+
+  }
+}
+
+const getObrasByEstado = async (req, res) =>{
+  try {
+    const {estado} = req.params
+
+    //buscar en la tabla estados, cual id corresponde al estado obtenido por parametros
+    // luego asignarlo a la constante idObtenido.
+    const estadoId = await pool.query('SELECT id FROM Estados WHERE nombre = $1', [estado])
+
+    if(estadoId.rows[0] === 0){
+      return res.status(404).json({ message: 'No existe el estado proporcionado'})
+    }
+
+    const idObtenido = estadoId.rows[0].id
+    //ahora se busca dentor de la tabla Obras, todas las obras que esten con el estado proporcionado por parametros
+
+    const obras = await pool.query('SELECT * FROM Obras WHERE estado_id = $1', [idObtenido])
+
+    //ver si hay obras con el estado proporcionado, caso contrario devolver un status 200, pero con un mensaje de no hay obras con el estado solicitado
+    if (obras.rows[0] === 0) {
+      return res.status(200).json({ message: 'No hay obras con el estado solicitado'})
+    }
+    
+    res.status(200).json({
+      success:true,
+      message: 'Obras encontradas correctamente',
+      obras: obras.rows
+    })
+
+
+
+  } catch (error) {
+    res.status(500).json({
+      success:false,
+      message: 'Error interno del servidor',
+      error: error.message
+    })
+  }
+}
+
+
 
 module.exports = {
     createObra,
     getAllObras,
-    
     darDeBajaObra,
     getObraByID,
-    modifyObra
+    modifyObra,
+    getObrasByUbicacion,
+    getObrasByEstado
 }   
--- routes/obra/obra.routes.js
+++ routes/obra/obra.routes.js
@@ -8,12 +8,19 @@ const { validarFechasObra } = require('../../middlewares/validarFechasObra.js');
 
 //controladores
 
-const {createObra, getAllObras,modificarObra,darDeBajaObra,modifyObra} = require('../../controllers/obra/obra.controller.js')
+const {createObra, getAllObras, modifyObra, darDeBajaObra, modifyObra, getObrasByEstado, getObraByID, getObrasByUbicacion} = require('../../controllers/obra/obra.controller.js')
 
 router.post('/create', validarFechasObra, createObra)
 router.get('/getAll', getAllObras)
-router.put('/modificar/:id', modifyObra) 
+router.put('/modificar/:id', validarFechasObra, modifyObra) 
 router.delete('/delete/:id', darDeBajaObra)
+
+router.get('/getById/:id', getObraByID)
+router.get('/getByEstado/:estado', getObrasByEstado)
+router.get('/getByUbicacion/:ubicacion', getObrasByUbicacion)
+
+
+
 
 
 module.exports = router;
