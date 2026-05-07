13d2924c1dbbe0de5b8707690132abbe4de4094e
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-05-06 23:01:23 -0300

    cambios en cors en todos los ms

--- microservices/ms-clientes.js
+++ microservices/ms-clientes.js
@@ -6,13 +6,13 @@ require('dotenv').config();
 const PORT = process.env.PORT_CLIENTES || 7010;
 
 // CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   if (req.method === 'OPTIONS') return res.sendStatus(200);
+//   next();
+// });
 
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url} - Microservicio de clientes`);
--- microservices/ms-materiales.js
+++ microservices/ms-materiales.js
@@ -7,13 +7,13 @@ const app = express();
 const PORT = process.env.MS_PORT || 7007;
 
 // CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   if (req.method === 'OPTIONS') return res.sendStatus(200);
+//   next();
+// });
 
 // Log
 app.use((req, res, next) => {
