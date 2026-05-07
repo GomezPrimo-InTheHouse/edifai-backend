8b3c039db427832624496c14f0801998ac2210d5
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-05-06 21:34:57 -0300

    cambios en db conecction 3

--- connection/db.js
+++ connection/db.js
@@ -1,14 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const connectionString = process.env.DATABASE_URL
-  ? decodeURIComponent(process.env.DATABASE_URL)
-  : undefined;
-
-console.log('Connecting to DB...');
-
 const pool = new Pool({
-  connectionString,
+  user: 'postgres.yfjlixegsswjnxxxbque',
+  password: process.env.PGPASSWORD,
+  host: 'aws-1-us-east-1.pooler.supabase.com',
+  port: 5432,
+  database: 'postgres',
   ssl: { rejectUnauthorized: false },
 });
 
