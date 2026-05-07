ed58873b9d2d06c2f003512e168c17fbf9cc3dc0
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-05-06 22:25:41 -0300

    cambios en db conecction 1

--- connection/db.js
+++ connection/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-console.log('PGPASSWORD:', process.env.PGPASSWORD);
-console.log('USER:', 'postgres.yfjlixegsswjnxxxbque');
-
 const pool = new Pool({
-  user: 'postgres.yfjlixegsswjnxxxbque',
+  user: 'postgres.mjhgxssercxlknfkfnjh',
   password: process.env.PGPASSWORD,
   host: 'aws-1-us-east-1.pooler.supabase.com',
   port: 5432,
