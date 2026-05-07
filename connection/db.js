b0affc559a13cba47cdb1c20e6f28e8149577c81
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-05-06 21:15:52 -0300

    cambios en db conecction

--- connection/db.js
+++ connection/db.js
@@ -1,20 +1,7 @@
-// // db.js
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   user: process.env.PGUSER,
-//   password: process.env.PGPASSWORD,
-//   host: process.env.PGHOST,
-//   port: process.env.PGPORT,
-//   database: process.env.PGDATABASE,
-// });
-
-// module.exports = pool;
-
-
 const { Pool } = require('pg');
 require('dotenv').config();
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
