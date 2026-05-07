beba764679435a226fa09464b144cb1afe850b6d
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-05-07 10:28:47 -0300

    cambios en cors de ms-noti

--- microservices/ms-notificaciones.js
+++ microservices/ms-notificaciones.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 const PORT = process.env.MS_PORT || 7009;
-
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
 app.use(express.json());
 
