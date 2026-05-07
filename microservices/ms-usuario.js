fa65176c807a3a5d3f9d6f72ba194e03c6f69274
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-05-06 22:52:43 -0300

    cambios en ms-usuario

--- microservices/ms-usuario.js
+++ microservices/ms-usuario.js
@@ -10,7 +10,7 @@ const usuariosRoutes = require('../routes/usuario/usuarios.routes.js');
 
 // CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173' || 'https://edifai-nuevo-frontend.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
