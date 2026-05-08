d1dad9733d41fb5bb48700ef8d0664e1805daade
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-05-08 10:31:31 -0300

    cambios en mddleware verificarTotpSiCorresponde 2

--- middlewares/verificarTotpSiCorresponde.js
+++ middlewares/verificarTotpSiCorresponde.js
@@ -1,40 +1,40 @@
-// middlewares/verificarTotpSiCorresponde.js
-const speakeasy = require('speakeasy');
+// // middlewares/verificarTotpSiCorresponde.js
+// const speakeasy = require('speakeasy');
  
-// const allowedRolesSinTotp = ['TRABAJADOR', 'TRABAJADOR JEFE']; // Roles que no requieren TOTP
+// // const allowedRolesSinTotp = ['TRABAJADOR', 'TRABAJADOR JEFE']; // Roles que no requieren TOTP
 
-const verificarTotpSiCorresponde = (req, res, next) => {
-  const { usuario } = req;
+// const verificarTotpSiCorresponde = (req, res, next) => {
+//   const { usuario } = req;
 
-  if (!usuario) {
-    return res.status(400).json({ error: 'Usuario no cargado en la solicitud' });
-  }
+//   if (!usuario) {
+//     return res.status(400).json({ error: 'Usuario no cargado en la solicitud' });
+//   }
 
-  if (usuario.rol_nombre === 'TRABAJADOR JEFE' || usuario.rol_id === 8) {
-    return next(); // Trabajador NO requiere TOTP
-  }
+//   if (usuario.rol_nombre === 'TRABAJADOR JEFE' || usuario.rol_id === 8) {
+//     return next(); // Trabajador NO requiere TOTP
+//   }
 
-  const { totp } = req.body;
+//   const { totp } = req.body;
 
-  if (!usuario.totp_seed) {
-    return res.status(403).json({ error: 'Usuario sin TOTP configurado' });
-  }
+//   if (!usuario.totp_seed) {
+//     return res.status(403).json({ error: 'Usuario sin TOTP configurado' });
+//   }
 
-  const verificado = speakeasy.totp.verify({
-    secret: usuario.totp_seed,
-    encoding: 'base32',
-    token: totp,
-    window: 1
-  });
+//   const verificado = speakeasy.totp.verify({
+//     secret: usuario.totp_seed,
+//     encoding: 'base32',
+//     token: totp,
+//     window: 1
+//   });
 
-  if (!verificado) {
-    return res.status(401).json({ error: 'Código TOTP inválido' });
-  }
+//   if (!verificado) {
+//     return res.status(401).json({ error: 'Código TOTP inválido' });
+//   }
 
-  next();
-};
+//   next();
+// };
 
-module.exports = verificarTotpSiCorresponde;
+// module.exports = verificarTotpSiCorresponde;
 
 
 const speakeasy = require('speakeasy');
