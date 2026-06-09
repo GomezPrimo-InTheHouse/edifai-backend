961bd656898d7d60745f4ff54b67e8d3a5fcfc43
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-06-09 18:49:41 -0300

    avances en labores 1.2

--- routes/labores/labores.routes.js
+++ routes/labores/labores.routes.js
@@ -1,52 +1,90 @@
-const express = require('express');
+// const express = require('express');
 
+// const router = express.Router();
+
+// //controladores
+
+// const {
+//     crearLabor,
+//     obtenerLabores,
+//     obtenerLaborPorId,
+//     actualizarLabor,
+//     darDeBajaLabor,
+//     cambiarEstadoLabor,
+//     obtenerMisLabores,
+//     obtenerLaboresPorObra,
+//     obtenerLaboresArchivadas
+// } = require ('../../controllers/labores/labores.controller.js')
+
+// //falta adicionar middlewares para verificar rol (solo los admin pueden acceder a estas rutas)
+
+// const {validarFechasObra, validarFechasRealesObra} = require('../../middlewares/validarFechasObra.js')
+// const {verificarToken} = require('../../middlewares/autorizacionDeRoles.js')
+
+// const {  verificar_estado,
+//     verificar_trabajador,
+//     verificar_especialidad,
+//     verificar_obra, 
+//     verificar_usuario} = require('../../middlewares/verificarExistencias.js')
+
+// router.post('/create',verificar_estado,
+//     verificar_trabajador,
+//     verificar_especialidad,
+//     verificar_obra, validarFechasObra, verificar_usuario, crearLabor);
+
+// router.get('/getAll', verificarToken, obtenerLabores);
+// router.get('/archivadas', verificarToken, obtenerLaboresArchivadas);
+
+// router.get('/getOne/:id', verificarToken, obtenerLaborPorId);
+// router.get('/mis-labores', verificarToken, obtenerMisLabores);
+
+// router.get('/getByObra/:obra_id', verificarToken, obtenerLaboresPorObra); 
+
+// router.put('/actualizarLabor/:id',verificarToken, verificar_estado,
+//     verificar_trabajador,
+//     verificar_especialidad,
+//     verificar_obra, validarFechasRealesObra, verificar_usuario , actualizarLabor);
+
+// router.delete('/darDeBaja/:id', verificarToken, darDeBajaLabor);
+// router.put('/cambiarEstadoLabor/:id', verificarToken, cambiarEstadoLabor)
+
+
+// module.exports = router;
+
+const express = require('express');
 const router = express.Router();
 
-//controladores
-
 const {
-    crearLabor,
-    obtenerLabores,
-    obtenerLaborPorId,
-    actualizarLabor,
-    darDeBajaLabor,
-    cambiarEstadoLabor,
-    obtenerMisLabores,
-    obtenerLaboresPorObra,
-    obtenerLaboresArchivadas
-} = require ('../../controllers/labores/labores.controller.js')
+    crearLabor, obtenerLabores, obtenerLaborPorId, actualizarLabor,
+    darDeBajaLabor, cambiarEstadoLabor, obtenerMisLabores,
+    obtenerLaboresPorObra, obtenerLaboresArchivadas
+} = require('../../controllers/labores/labores.controller.js');
 
-//falta adicionar middlewares para verificar rol (solo los admin pueden acceder a estas rutas)
+const { verificarToken } = require('../../middlewares/autorizacionDeRoles.js');
+const { validarFechasObra, validarFechasRealesObra } = require('../../middlewares/validarFechasObra.js');
+const { verificar_estado, verificar_trabajador, verificar_especialidad, verificar_obra, verificar_usuario } = require('../../middlewares/verificarExistencias.js');
 
-const {validarFechasObra, validarFechasRealesObra} = require('../../middlewares/validarFechasObra.js')
-const {verificarToken} = require('../../middlewares/autorizacionDeRoles.js')
-
-const {  verificar_estado,
-    verificar_trabajador,
-    verificar_especialidad,
-    verificar_obra, 
-    verificar_usuario} = require('../../middlewares/verificarExistencias.js')
-
-router.post('/create',verificar_estado,
-    verificar_trabajador,
-    verificar_especialidad,
-    verificar_obra, validarFechasObra, verificar_usuario, crearLabor);
+router.post('/create',
+    verificarToken,
+    verificar_estado, verificar_trabajador, verificar_especialidad,
+    verificar_obra, validarFechasObra, verificar_usuario,
+    crearLabor
+);
 
 router.get('/getAll', verificarToken, obtenerLabores);
 router.get('/archivadas', verificarToken, obtenerLaboresArchivadas);
-
-router.get('/getOne/:id', verificarToken, obtenerLaborPorId);
 router.get('/mis-labores', verificarToken, obtenerMisLabores);
+router.get('/getByObra/:obra_id', verificarToken, obtenerLaboresPorObra);
+router.get('/getOne/:id', verificarToken, obtenerLaborPorId);
 
-router.get('/getByObra/:obra_id', verificarToken, obtenerLaboresPorObra); 
-
-router.put('/actualizarLabor/:id',verificarToken, verificar_estado,
-    verificar_trabajador,
-    verificar_especialidad,
-    verificar_obra, validarFechasRealesObra, verificar_usuario , actualizarLabor);
+router.put('/actualizarLabor/:id',
+    verificarToken,
+    verificar_estado, verificar_trabajador, verificar_especialidad,
+    verificar_obra, validarFechasRealesObra, verificar_usuario,
+    actualizarLabor
+);
 
 router.delete('/darDeBaja/:id', verificarToken, darDeBajaLabor);
-router.put('/cambiarEstadoLabor/:id', verificarToken, cambiarEstadoLabor)
-
+router.put('/cambiarEstadoLabor/:id', verificarToken, cambiarEstadoLabor);
 
 module.exports = router;
