f189afca9665942e820d4e84db87d070156e1929
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-06-10 10:08:25 -0300

    avances en labores 1.7.1

--- middlewares/validarFechasObra.js
+++ middlewares/validarFechasObra.js
@@ -1,30 +1,29 @@
 const validarFechasObra = (req, res, next) => {
-  const {  fecha_fin_estimado, 
-            fecha_inicio_estimado, } = req.body;
+  const { fecha_fin_estimada, fecha_inicio_estimada } = req.body;
 
-  if (new Date(fecha_inicio_estimado) >= new Date(fecha_fin_estimado)) {
-    return res.status(400).json({
-      error: 'La fecha de inicio debe ser anterior a la fecha de fin',
-    });
+  if (fecha_inicio_estimada && fecha_fin_estimada) {
+    if (new Date(fecha_inicio_estimada) >= new Date(fecha_fin_estimada)) {
+      return res.status(400).json({
+        error: 'La fecha de inicio debe ser anterior a la fecha de fin',
+      });
+    }
   }
 
   next();
 };
 
 const validarFechasRealesObra = (req, res, next) => {
-  
-  const {  fecha_fin_real, 
-            fecha_inicio_real, } = req.body;
+  const { fecha_fin_real, fecha_inicio_real } = req.body;
 
-  if (new Date(fecha_inicio_real) >= new Date(fecha_fin_real)) {
-    return res.status(400).json({
-      error: 'La fecha de inicio debe ser anterior a la fecha de fin',
-    });
+  if (fecha_inicio_real && fecha_fin_real) {
+    if (new Date(fecha_inicio_real) >= new Date(fecha_fin_real)) {
+      return res.status(400).json({
+        error: 'La fecha de inicio debe ser anterior a la fecha de fin',
+      });
+    }
   }
 
   next();
 };
 
-
-
-module.exports = {validarFechasObra, validarFechasRealesObra};  
+module.exports = { validarFechasObra, validarFechasRealesObra };
